9fd9b3e0396df72fc2e9150ec1ba7293f47fbda6
Sriram8055 <124173905+Sriram8055@users.noreply.github.com>
2025-01-08 00:48:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom/client"; // Updated import
+import App from "./App";
 
-reportWebVitals(sendToVercelAnalytics);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
